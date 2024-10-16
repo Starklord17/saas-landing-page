aabc3e1a763b24d733ae3df094ed4550efd10be3
Starklord17 <maxi.sm117@gmail.com>
2024-10-16 20:24:30 -0300

    refactor: Update Features section and add header navigation
    - Update Features.tsx to import additional details from constants
    - Add a new unordered list with details in the Features section
    - Style the new list with appropriate classes
    - Update Header.tsx to include a scroll event listener
    - Add a new NavLink component to handle navigation links
    - Update the Header component to conditionally apply styles on scroll

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
-import clsx from "clsx";
+import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
+import clsx from "clsx";
 
 interface NavLinkProps {
   title: string;
   to: string;
 }
 
-const NavLink = ({ title, to }: NavLinkProps) => (
-  <LinkScroll
-    className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
-    to={to}
-    smooth={true}
-    duration={500}
-  >
-    {title}
-  </LinkScroll>
-);
-
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const NavLink = ({ title }: NavLinkProps) => (
+    <LinkScroll
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+    >
+      {title}
+    </LinkScroll>
+  );
 
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
@@ -38,13 +56,22 @@ const Header = () => {
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
-                  <NavLink title="Features" to="features-section" />
+                  <NavLink title="features" to="features-section" />
                   <div className="dot" />
-                  <NavLink title="Pricing" to="pricing-section" />
+                  <NavLink title="pricing" to="pricing-section" />
                 </li>
 
                 <li className="nav-logo">
-                  <LinkScroll to="hero" offset={-100} spy smooth duration={500} className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}>
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    duration={500}
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
+                  >
                     <img
                       src="/images/xora.svg"
                       width={160}
@@ -63,10 +90,21 @@ const Header = () => {
             </nav>
 
             <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380] translate-x-[-290px] -translate-y-1/2 rotate-90">
-              <img src="/images/bg-outlines.svg" width={960} height={380} alt="outline" className="relative z-2" />
-              <img src="/images/bg-outlines-fill.png" width={960} height={380} alt="outline" className="absolute inset-0 mix-blend-soft-light opacity-5" />
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
             </div>
-
           </div>
         </div>
 
