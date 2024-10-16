956cc55d237d66aad677879ba45a2ad63f4d59cf
Starklord17 <maxi.sm117@gmail.com>
2024-10-16 00:02:04 -0300

    :zap: Hero section

--- saas_app/src/App.tsx
+++ saas_app/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './sections/Header.js';
+import Hero from './sections/Hero.js';
 
 const App = () => {
   return (
     <main className="overflow-hidden">
-      <Header></Header>
+      <Header />
+      <Hero />
     </main>
   )
 }
