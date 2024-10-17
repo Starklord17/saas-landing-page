0806f2014c18ddd3ef668ef47b442afad30fdd21
Starklord17 <maxi.sm117@gmail.com>
2024-10-17 17:38:05 -0300

    Add Testimonials section with TestimonialItem component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero.js';
 import Features from './sections/Features.js';
 import Pricing from './sections/Pricing.js';
 import Faq from './sections/faq.js';
+import Testimonials from './sections/Testimonials.js';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   )
 }
